f9fd8bfca7bc75a2c770dd393e3950ae3cc2ce93
James Roll <jcroll@fullsail.edu>
2014-10-31 18:03:41 -0400

    Day 3 homework expressions
    Expressions commit

--- Day 3/Day3LabExpressions/scriptTwo.js
+++ Day 3/Day3LabExpressions/scriptTwo.js
@@ -5,3 +5,27 @@
 alert("Grade Gently");
 
 
+//Slices of Pie part 1
+
+//Given:
+var slices = 4;
+var people = 8;
+var pizzas = 4;
+//Result Variables:
+var slicesEa = pizzas*slices/people;
+console.log(slicesEa);
+//Result Format:
+console.log("Each person ate " + slicesEa + " slices of pizza at the party.");
+
+
+//Slices of Pie part 2
+
+//Given:
+var slices = 4;
+var people = 8;
+var pizzas = 4;
+//Results:
+var sparkyGot = slicesEa*pizzas%people;
+console.log(sparkyGot);
+//Result Format
+console.log ()
