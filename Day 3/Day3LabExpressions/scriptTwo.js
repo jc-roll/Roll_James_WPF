349a08b18ea020075046636a0a1ae97dffefbdda
James Roll <jcroll@fullsail.edu>
2014-10-31 18:27:59 -0400

    Homework day 3
    committing things is awesome

--- Day 3/Day3LabExpressions/scriptTwo.js
+++ Day 3/Day3LabExpressions/scriptTwo.js
@@ -28,4 +28,26 @@ var pizzas = 4;
 var sparkyGot = slicesEa*pizzas%people;
 console.log(sparkyGot);
 //Result Format
-console.log ()
+console.log ("Sparky got " + sparkyGot + " slices of pizza at the party")
+
+
+//Slices of Pie part 2
+
+//Given:
+var week1 = 50;
+var week2 = 100;
+var week3 = 100;
+var week4 = 100;
+var week5 = 50;
+var time = 5;
+//Result Variable
+var total = week1+week2+week3+week4+week5;
+var average = total/time;
+console.log(average);
+//Result Format:
+console.log ("You have spent a total of $" + total + " on groceries over 5 weeks. That is an average of $" + average + " per week");
+
+//Discounts:
+
+//Givens:
+var originalP =5;
