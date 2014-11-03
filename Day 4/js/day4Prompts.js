cd29f007d92abb081582896891a96c88754693ca
James Roll <jcroll@fullsail.edu>
2014-11-03 18:51:14 -0500

    Calculated Age based on year
    Learning about prompts

---
+++ Day 4/js/day4Prompts.js
@@ -0,0 +1,18 @@
+/*
+James Roll
+Day 4 Prompts
+Wpf 1411
+11-3-2014
+ */
+
+//alert("testing 1.2,3!");
+
+//ask user for input
+
+//prompt() = ask the user to type something in
+var yearBorn = prompt("Enter your year of Birth");
+console.log(yearBorn);
+
+//figure out how old we are
+var age = 2014 - yearBorn;
+console.log("You are "+age+" years old. ")
