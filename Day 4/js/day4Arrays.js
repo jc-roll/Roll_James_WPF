96a8c84e8ea1b5e1d6721da0c88aadd91b4bde32
James Roll <jcroll@fullsail.edu>
2014-11-03 18:31:36 -0500

    Finished Array file
    We now know every thing about oranges

--- Day 4/js/day4Arrays.js
+++ Day 4/js/day4Arrays.js
@@ -29,3 +29,37 @@ console.log(avengersNames.length);
 
 avengersNames[avengersNames.length] = "Captain America";
 console.log(avengersNames);
+
+//Add Black Widow to the mix
+avengersNames[avengersNames.length]= "Black Widow";
+
+//access last avenger in the array
+console.log(avengersNames[avengersNames.length -1]);
+
+//New method specific to arrays called push
+//it adds what ever we put in automatically to the end of our array.
+avengersNames.push("Hawkeye");
+console.log(avengersNames);
+
+//pop() - method of an array that removes the last item in an array
+// you can save the pop'd variable
+// set a variable equal to the pop method
+
+var soloAvenger = avengersNames.pop();
+console.log(avengersNames);
+console.log(soloAvenger);
+
+//Pick Oranges every day
+//Track how many Oranges are picked in each day
+var orangeBins=[234, 473, 20, 709];
+
+//How many oranges where picked in total
+//Store that in a variable
+var orangeTotal = orangeBins[0] + orangeBins[1] + orangeBins[2] + orangeBins[3];
+console.log(orangeTotal+" is the total number of oranges picked. ");
+
+//What is the average oranges picked?
+var orangeAverage = orangeTotal/orangeBins.length;
+console.log("The average amount of oranges picked in" +orangeBins.length+" days is "+orangeAverage+ ".");
+
+//add a day
