b03700d019186c3802588d6eedfbc928927ad193
James Roll <jcroll@fullsail.edu>
2014-11-03 19:08:26 -0500

    finished prompting
    included calculation rectangle area prompts

--- Day 4/js/day4Prompts.js
+++ Day 4/js/day4Prompts.js
@@ -16,3 +16,21 @@ console.log(yearBorn);
 //figure out how old we are
 var age = 2014 - yearBorn;
 console.log("You are "+age+" years old. ")
+
+//Calculate the area of a rectangle
+// area= length* width
+
+var length = prompt(" We are going to calculate the area of a rectangle. \nPlease enter the length of your rectangle:");
+var width = prompt("Please enter the width of your rectangle:");
+//now lets calculate
+var area =length*width;
+
+var results =("The area of the rectangle with a width of "+width+ " and a length of "+length+" is "+area+".");
+
+//console the area
+console.log("The area of the rectangle with a width of "+width+ " and a length of "+length+" is "+area+".");
+
+console.log(results);
+
+alert("The area of the rectangle with a width of "+width+ " and a length of "+length+" is "+area+".");
+//by adding alert to the front of the concatenation it shoots it back at the user
