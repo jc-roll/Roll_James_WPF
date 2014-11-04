d355243b0140f1a00ba765d81f7b1d78183c0945
James Roll <jcroll@fullsail.edu>
2014-11-03 23:50:51 -0500

    Work Related update
    working on increasing size

--- Expressions_Assignment/js/workRelated.js
+++ Expressions_Assignment/js/workRelated.js
@@ -10,9 +10,10 @@ Wpf 1411
 
 //prompts
 var hoursDay = prompt("How many hours do you use your computer in a day?");
-var daysWeek = prompt ("How many days do you work in a week?\n On average don't think holidays (Must be a whole number).")
-
-
- console.log(computerTypes[1]);
+var daysWeek = prompt("How many days do you work in a week?\n On average don't think holidays (Must be a whole number).");
+var sickDays = prompt("how many sick days do you plan on using this year?");
+var totalDay = daysWeek*52-sickDays;
+var totalHours = hoursDay*daysWeek*52;
+console.log(totalHours);
 
 
