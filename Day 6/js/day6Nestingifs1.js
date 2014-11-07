8faeaa883c7498ecd0330a29097615e7468f6975
James Roll <jcroll@fullsail.edu>
2014-11-07 18:22:41 -0500

    Nesting if STatments
    Finsihed

--- Day 6/js/day6Nestingifs1.js
+++ Day 6/js/day6Nestingifs1.js
@@ -12,27 +12,61 @@ Wpf 1411
  //If it is warm enough lets go to the beach if not lets go to the movies
  //If the water is warm enough lets go swimming
  //if not lets lay out and get a tan
+ //Choose kids movie or horror - are you taking a kid
 
  //Get temp
- var temp = 89;
+ var temp = prompt("Whats the temp out side?");
  //get water temp
- var waterTemp = 80;
+ var waterTemp = 60;
+ //Kid?
+ var child = true;
 
  //If else
  if(temp>70){
   console.log("Lets go to the beach!");
 
- } else {
-  console.log("Lets go to the movies!")
+  waterTemp = prompt("What is the water temp?");
+  //test water temp
+  if(waterTemp>75){
+   console.log("Let's go swimming in the water");
 
+  } else{
+   console.log("Let's lay out and get a tan");
+
+  }
+ } else {
+  console.log("Lets go to the movies!");
+
+  if(child){
+   var childAge = prompt("How old is the child?");
+
+   if(childAge<=6){
+    console.log("Let's see the smurf movie");
+
+   }else{
+    console.log("Go see indiana jones movie");
+
+   }
+   console.log("Go see kids movie");
+
+  } else{
+   console.log("Bring the blood horror movie");
+
+  }
  }
 
- //test water temp
 
- if(waterTemp>75){
-  console.log("Let's go swimming in the water")
+ // using && and || or
 
- } else{
-  console.log("Let's lay out and get a tan")
+ if(temp>70 && waterTemp>75){
+  console.log("Let's go to the beach!\n Let's go swimming");
 
+ }else if(temp>70 && waterTemp<=75){
+  console.log("Let's go the beach! \n Let's get a tan!");
+
+ }else if(temp<=70 && child){
+  console.log("Let's go to the movies!\n Let's see a animated movie!");
+
+ }else if(temp<=70 && !child) {
+  console.log("Let's go to the movies!\n Let's see a horror movie!");
  }
