2d56b1e8078c6ae98e06195d7e00d641ef3cae1f
James Roll <jcroll@fullsail.edu>
2014-11-07 23:21:03 -0500

    Conditionals Industry
    updated almost done

--- Conditonals_Industry/js/conditionalsIndustry.js
+++ Conditonals_Industry/js/conditionalsIndustry.js
@@ -6,4 +6,20 @@ Wpf 1411
 Date
  */
 
-alert("testing 1.2,3!");
+//alert("testing 1.2,3!");
+
+// this is a code that is about how when you go to a store for a new computer if you qualify for a discount and if you do what kind.
+ var age = prompt("How old are you?");
+ var timeInCompany = prompt("How many years have you worked for the company?");
+ var ageDiscount;
+
+
+
+ ageDiscount = (age<50)? "You are to young to get the senior discount" : (age>=50)? "You are old enough to get the senior discount!";
+ console.log(book);
+
+ if(timeInCompany>10){
+  console.log("You get a company discount because you have worked here for " +timeInCompany+ " years.");
+
+ } else{
+  console.log("You don't get a company discount because you have not worked here for at least ten years.\nYou only have " +timeInCompany+ );
