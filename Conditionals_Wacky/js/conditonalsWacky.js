5510824b58a6dde5ec143eda6c03a0cf0947a7a5
James Roll <jcroll@fullsail.edu>
2014-11-07 23:02:23 -0500

    Conditionals Wacky
    Finalized the wacky conditionals

--- Conditionals_Wacky/js/conditonalsWacky.js
+++ Conditionals_Wacky/js/conditonalsWacky.js
@@ -18,22 +18,17 @@ Wpf 1411
  clownQuestions[1] = prompt("Hello clown two first question do you like chocolate?")
  clownQuestions[2] = prompt("Hello clown one second question do you like other clowns?")
  clownQuestions[3] = prompt("hello clown two second question do you like other clowns?")
-var no = 1
-if(clownQuestions[2] == 1){
+
+if(clownQuestions[2].toLowerCase() == "no" || clownQuestions[3].toLowerCase() == "no"){
  console.log("You clowns are not a match best find some other clown to sit with you might fight!")
 
-} else if(clownQuestions[3] == 1) {
- console.log("You clowns are not a match best find some other clown to sit with you might fight!")
-
-} else if (clownQuestions[2] == 1){
-  console.log("You clowns are not a match you both don't like chocolate!")
-
- } else if(clownQuestions[3] == 1){
+} else if (clownQuestions[2] == "no" || clownQuestions[3] == "no"){
   console.log("You clowns are not a match you both don't like chocolate!")
 
 }else if((clownQuestions[0] == clownQuestions[1]) && (clownQuestions[2] == clownQuestions[3])){
-  console.log("Alright congrats you clowns are a match go eat all the chocolate you can stomach!");
+  console.log("Alright congrat clowns you are a match go eat all the chocolate you can stomach!");
 
  } else {
  console.log("You clowns are not a match best find some other clown to sit with you might fight!")
  }
+ console.log(clownQuestions);
