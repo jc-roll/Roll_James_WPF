ad8622a7f5b9821105758857e803195676387812
James Roll <jcroll@fullsail.edu>
2014-11-12 19:26:49 -0500

    Starting parameters and arguments
    Starting parameters and arguments

--- Day8/js/day8Arguments.js
+++ Day8/js/day8Arguments.js
@@ -1,9 +1,25 @@
 
  /*
 James Roll
-Title
+Arguments
 Wpf 1411
-Date
+11-12-2014
  */
 
-alert("testing 1.2,3!");
+//alert("testing 1.2,3!");
+
+ //argument = goes into the function - part of the function call
+ //Parameter - catches the arguments and is used inside the function
+
+ //create function that calculates the area of a rectangle
+
+ function calcArea(){
+
+  var width = 10;
+  var height = 20;
+  var area = width*height;
+
+  console.log("The area of your rectangle is "+area);
+
+ }
+ calcArea();
