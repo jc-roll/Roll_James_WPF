23040db452bae83abe45727565a857a0fef87561
James Roll <jcroll@fullsail.edu>
2014-11-12 20:01:30 -0500

    finished arguments and parameters
    finished arguments and parameters

--- Day8/js/day8Arguments.js
+++ Day8/js/day8Arguments.js
@@ -13,13 +13,29 @@ Wpf 1411
 
  //create function that calculates the area of a rectangle
 
- function calcArea(){
+ function calcArea(width,height){
 
-  var width = 10;
-  var height = 20;
+  //var width = 10;
+  //var height = 20;
   var area = width*height;
 
   console.log("The area of your rectangle is "+area);
 
  }
- calcArea();
+ calcArea(4,5);
+ calcArea(10,10);
+ var width = prompt("width?");
+ var height = prompt("height?");
+ calcArea(width,height);
+
+
+ //calculate dog years
+ //dog years =  human years times 7
+
+ function dogYears(humanAge){
+  var dogAge = humanAge*7;
+  console.log("You are "+dogAge+" years old in dog years!");
+ }
+ dogYears(36);
+ var yourAge = prompt("How old are you?");
+ dogYears(yourAge);
