d420b30cfd3c968ad5158303ae74abde52136eee
James Roll <jcroll@fullsail.edu>
2014-11-14 19:04:33 -0500

    Day 9 Procedures
    Day 9 Procedures

--- Day9/js/day9Procedures.js
+++ Day9/js/day9Procedures.js
@@ -8,4 +8,30 @@ Wpf 1411
 
 //alert("testing 1.2,3!");
 
- 
+//basic function
+ function calcArea(width,height){
+  var area = width*height;
+
+  return area;
+
+ }
+
+ //function call
+ var returnedArea = calcArea(6,7);
+
+ console.log(returnedArea);
+
+ /////////////////////////////////////////////////////////////////////////////
+
+ //Procedure - console.log the perimeter
+
+ function calcPeri(width,height){
+  var peri = width*2 + height*2;
+
+  console.log(peri);
+
+ }
+
+ //do not catch with a variable
+
+ calcPeri(6,7);
